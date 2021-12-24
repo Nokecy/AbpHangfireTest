41a4a3edfa5cbb5504385996b7813bc4b65fb1bb
nokecy <nokecy@163.com>
2021-12-25 00:03:13 +0800

    添加项目文件。

---
+++ src/Test.HttpApi/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Test.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Test.Controllers
+{
+    /* Inherit your controllers from this class.
+     */
+    public abstract class TestController : AbpControllerBase
+    {
+        protected TestController()
+        {
+            LocalizationResource = typeof(TestResource);
+        }
+    }
+}
---
+++ src/Test.Web/TestBrandingProvider.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Ui.Branding;
+using Volo.Abp.DependencyInjection;
+
+namespace Test.Web
+{
+    [Dependency(ReplaceServices = true)]
+    public class TestBrandingProvider : DefaultBrandingProvider
+    {
+        public override string AppName => "Test";
+    }
+}
